bc5f7df469b1442fd26a71ae43c65ae339768982
Arthur <kingart808@gmail.com>
2019-08-09 11:22:32 -1000

    some people just want to see the world burn

---
+++ server.js
@@ -0,0 +1,111 @@
+const http = require('http');
+const fs = require('fs');
+
+const PORT = 8080;
+
+const server = http.createServer((req, res) => {
+
+  console.log(req.method);
+  console.log(req.url);
+  console.log(req.headers);
+
+  // body???
+
+  let body = '';
+  req.on('data', (chunk) => {
+  let test = chunk.toString()
+    body += chunk;
+  });
+// let urlRequest = `./public`
+  req.on('end', () => {
+    getHandler(req,res)
+  //   if(req.url === `/` || req.url === `/index.html`){
+  //   fs.readFile(`./public/index.html`,function (err,data) {
+  //     if (err){
+  //       return console.log('could not read the file')
+  //     }
+      
+  //     res.writeHead(200, {
+  //       'content-type': 'text/html',
+  //       'content-length': data.length,
+  //     });
+  //     res.write(data.toString())
+  //     res.end();
+  //   });
+  // }
+  // });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started on PORT: ${PORT}`);
+});
+function getHandler(req,res) {
+  if(req.url === `/` || req.url === `/index.html`){
+    fs.readFile(`./public/index.html`, function (err,data) {
+      if(err){
+        getHandler(data, `./public/404.html`)
+      }else{
+        res.writeHead(200,{
+          'content-type': 'text/html',
+          'content-length': data.length,
+        })
+        res.write(data.toString())
+        res.end()
+      }
+    })
+  }
+}
+
+// function getHandler(req, res) {
+//   fs.readFile(`./public/index.html`,function (err,data) {
+//     if (err){
+//       getHandler(arg1, '/404.html')
+//     } else {
+//       res.writeHead(200, {
+//         'content-type': 'text/html',
+//         'content-length': data.length,
+//       });
+//       res.write(data.toString())
+//       res.end();
+//     }
+  
+// }
+
+// fs.readFile('./test.txt', (err, data) => {
+  //   if (err) {
+  //     return console.log('could not write the file');
+  //   }
+  //   console.log(data);
+  // });
+
+// req.on('end', () => {
+// //  figure out newElement.html string data
+//  // using the contents of the request
+
+//   // write the newElement.html file
+//   fs.writeFile('./newElement.html', (err, data) => {
+//     if (err) {
+//       return console.log(err);
+//     }
+
+//     // read contents of the public dir
+//     fs.readdir('./', (err, dir) => {
+//       if (err) {
+//         return console.log(err);
+//       }
+
+//       // figure out new index.html
+//       const index = '<html></html>';
+
+//       // write new index.html
+//       fs.writeFile('./index.html', index, (err) => {
+//         if (err) {
+//           return console.log(err);
+//         }
+
+//         res.end();
+//       });
+//     });
+//   });
+// });
+})
