b3920cd67bbd809a9245452a035d52c3daf9bea3
Arthur <kingart808@gmail.com>
2019-08-10 18:24:01 -1000

    get & post done

--- server.js
+++ server.js
@@ -1,104 +1,187 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
+const querystring = require("querystring");
 
 const PORT = 8080;
 
+function newPost(reqParse) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>The Elements - ${reqParse.elementName}</title>
+    <link rel="stylesheet" href="/css/styles.css">
+  </head>
+  <body>
+    <h1>${reqParse.elementName}</h1>
+    <h2>${reqParse.elementSymbol}</h2>
+    <h3>Atomic number ${reqParse.elementAtomicNumber}</h3>
+    <p>${reqParse.elementDescription}</p>
+    <p><a href="/">back</a></p>
+  </body>
+  </html>`;
+}
 const server = http.createServer((req, res) => {
-
   console.log(req.method);
   console.log(req.url);
   console.log(req.headers);
 
   // body???
 
-  let body = '';
-  req.on('data', (chunk) => {
-  let test = chunk.toString()
+  let body = "";
+  req.on("data", chunk => {
+    let test = chunk.toString();
     body += chunk;
   });
-// let urlRequest = `./public`
-  req.on('end', () => {
-    getHandler(req,res)
-  //   if(req.url === `/` || req.url === `/index.html`){
-  //   fs.readFile(`./public/index.html`,function (err,data) {
-  //     if (err){
-  //       return console.log('could not read the file')
-  //     }
-      
-  //     res.writeHead(200, {
-  //       'content-type': 'text/html',
-  //       'content-length': data.length,
-  //     });
-  //     res.write(data.toString())
-  //     res.end();
-  //   });
-  // }
-  // });
+
+  req.on("end", () => {
+    if (req.method === `POST`) {
+      postHandler(req, res, body);
+    } else {
+      getHandler(req, res);
+    }
+  });
 });
-})
+
 server.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);
 });
 
-function getHandler(req,res) {
-  if(req.url === `/` || req.url === `/index.html`){
-    fs.readFile(`./public/index.html`, function (err,data) {
-      if(err){
-        errorHandler(data)
-      }else{
-        res.writeHead(200,{
-          'content-type': 'text/html',
-          'content-length': data.length,
-        })
-        res.write(data.toString())
-        res.end()
+function getHandler(req, res) {
+  if (req.url === `/` || req.url === `/index.html`) {
+    fs.readFile(`./public/index.html`, function(err, data) {
+      if (err) {
+        errorHandler(res);
+      } else {
+        res.writeHead(200, {
+          "content-type": "text/html",
+          "content-length": data.length
+        });
+        res.write(data.toString());
+        res.end();
       }
-    })
-  }else if (req.url === `/css/styles.css`){
-    fs.readFile(`./public/css/styles.css`,function (err,data) {
-      if(err){
-        getHandler(data,`./public/404.html`)
-      }else{
-        res.writeHead(200,{
-          'content-type': 'text/css',
-          'content-length': data.length,
-        })
-        res.write(data.toString())
-        res.end()
+    });
+  } else if (req.url === `/css/styles.css`) {
+    fs.readFile(`./public/css/styles.css`, function(err, data) {
+      if (err) {
+        errorHandler(res);
+      } else {
+        res.writeHead(200, {
+          "content-type": "text/css",
+          "content-length": data.length
+        });
+        res.write(data.toString());
+        res.end();
       }
-    })
-  }else{
-    fs.readFile(`./public${req.url}`,function (err,data) {
-      if(err){
-        getHandler(data,`./public/404.html`)
-      }else{
-        res.writeHead(200,{
-          'content-type': 'text/html',
-          'content-length': data.length,
-        })
-        res.write(data.toString())
-        res.end()
+    });
+  } else {
+    fs.readFile(`./public${req.url}`, function(err, data) {
+      if (err) {
+        errorHandler(res);
+      } else {
+        res.writeHead(200, {
+          "content-type": "text/html",
+          "content-length": data.length
+        });
+        res.write(data.toString());
+        res.end();
       }
-    })
+    });
   }
 }
+
+
 function errorHandler(res) {
-  fs.readFile(`./public/404.html`,function (data) {
-    res.writeHead(404,{
-      'content-type': 'text/html',
-      'content-length': data.length
-    })
-    res.write(data.toString())
-    res.end()
-  })
+  fs.readFile(`./public/404.html`, function(err, data) {
+    if (err) {
+      res.writeHead(500, {
+        "content-type": "application/json"
+      });
+    }
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "content-length": data.length
+    });
+    res.write(data.toString());
+    res.end();
+  });
 }
-    
+
+function postHandler(req, res, body) {
+  let reqParse = querystring.parse(body);
+  fs.writeFile(
+    `./public/${reqParse.elementName}.html`,
+    newPost(reqParse),
+    (err, data) => {
+      if (err) {
+        return errorHandler(res);
+      }
+
+      fs.readdir(`./public`, function(err, data) {
+        if (err) {
+          errorHandler(res);
+        } else {
+          let filtered = data.filter(function(e) {
+            return ![`.keep`, `404.html`, `css`, `index.html`].includes(e);
+          });
+
+          fs.writeFile(`./public/index.html`, makeIndex(filtered), function (err) {
+            if(err){
+              errorHandler(res)
+            }
+          })
+
+
+          let str = `{ "success" : true }`;
+          res.writeHead(200, {
+            "content-type": "application/json",
+            "content-length": str.length
+          });
+          res.write(str);
+          res.end();
+        }
+      });
+    }
+  );
+}
+
+
+function makeIndex(filtered) {
+ let idx = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>The Elements</title>
+    <link rel="stylesheet" href="/css/styles.css">
+  </head>
+  <body>
+    <h1>The Elements</h1>
+    <h2>These are all the known elements.</h2>
+    <h3>These are ${filtered.length}</h3>
+    <ol>`
+      
+  let idx2 =  `
+  </ol>
+  </body>
+  </html>`
+  for (let i  = 0; i < filtered.length; i++) {
+    let name = filtered[i].split('.')
+    let listElements = `<li>
+    <a href= "/${filtered[i]}">${name[0]}</a>
+    </li>` 
+    idx += listElements
+  }
+  idx += idx2
+  return idx
+  }
+
+  
 // fs.readFile('./test.txt', (err, data) => {
-  //   if (err) {
-  //     return console.log('could not write the file');
-  //   }
-  //   console.log(data);
-  // });
+//   if (err) {
+//     return console.log('could not write the file');
+//   }
+//   console.log(data);
+// });
 
 // req.on('end', () => {
 // //  figure out newElement.html string data
